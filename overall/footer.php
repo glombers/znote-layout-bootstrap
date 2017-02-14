e6a4164d8c49a65be1864da3fccb6403e2a65b05
Webus <igames241@gmail.com>
2017-02-14 08:16:12 +0100

    Add files via upload

---
+++ overall/footer.php
@@ -0,0 +1,12 @@
+			</div>
+		</div>
+		<div class="panel panel-default">
+			<div class="panel-heading" style="text-align: center;">
+				<br/><b>Created by:</b> <a href="https://otland.net/members/webo.192791/">Webo</a>. <b>Engine:</b> <a href="https://github.com/Znote/ZnoteAAC">Znote AAC</a></p>
+			</div>
+		</div>
+		<script src="layout/js/bootstrap.min.js"></script>
+		<script src="layout/js/jquery.countdown.min.js"></script>
+		<script src="layout/js/misc.js"></script>
+	</body>
+</html>
