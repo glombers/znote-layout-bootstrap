f7701ef2c55c7019360a57b8ad5f066f29bc8162
Webus <igames241@gmail.com>
2017-02-14 13:02:55 +0100

    Update header.php

--- overall/header.php
+++ overall/header.php
@@ -13,19 +13,15 @@
 		<meta http-equiv="content-language" content="en">
 		<meta name="description" content="Tibia is a free massively multiplayer online role-playing game (MMORPG)">
 		<meta name="keywords" content="opentibia, free online rpg, free mmorpg, mmorpg, mmog, online role playing game, online multiplayer game, internet game, online rpg, rpg">
-
 		<!-- Icons -->
 		<link rel="shortcut icon" href="layout/images/favicon.gif" />
-
 		<!-- CSS -->
 		<link rel="stylesheet" type="text/css" href="layout/css/style.css">
 		<link rel="stylesheet" type="text/css" href="layout/css/bootstrap.css">
 		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-
 		<!-- JS -->
 		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
 	</head>
-
 	<body>
 		<div id="container">
 			<div class="header"></div>
@@ -36,7 +32,6 @@
 					</div>
 				</div>
 			</nav>
-
 			<div class="sidebar">
 				<?php
 					if (user_logged_in() && is_admin($user_data)) include 'layout/widgets/Wadmin.php'; 				
@@ -45,7 +40,6 @@
 					include('layout/widgets/serverstatus.php');
 				?>
 			</div>
-
 			<div class="content">
 				<div class="panel panel-default">
 					<div class="panel-body">
